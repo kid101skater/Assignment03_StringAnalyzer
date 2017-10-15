85247f2ede6327809f82a3656e63a187deced4cc
kid101skater <klocke@mail.greenriver.edu>
2017-10-15 10:29:12 -0700

    Fix functionality to move back from the analyzer activity to new input activity

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
@@ -1,10 +1,14 @@
 package co.miniforge.corey.stringanalyzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.R.id.input;
 
 
 public class AnalyzerActivity extends AppCompatActivity {
@@ -35,6 +39,8 @@ public class AnalyzerActivity extends AppCompatActivity {
         locateViews();
 
         bindData();
+
+        bindFunctionality();
     }
 
     void locateViews() {
@@ -65,5 +71,16 @@ public class AnalyzerActivity extends AppCompatActivity {
         longestWord.setText(String.format("Longest Word: %s", textUtil.getLongestWord()));
         uniqueWords.setText(String.format("Unique Words: %d", textUtil.getUniqueWords()));
         specialCharactersCount.setText(String.format("Special Characters: %d", textUtil.getSpecialCharacterCount()));
+    }
+
+    void bindFunctionality(){
+        backToInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    Intent intent = new Intent(getApplicationContext(), InputActivity.class);
+                    startActivity(intent);
+            }
+        });
     }
 }
